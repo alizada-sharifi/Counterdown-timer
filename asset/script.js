3833e446b6eeedf30d58c7bce41e62dd76c425b6
A.Sharifi <alizadasharifi2022@gmail.com>
2024-10-12 17:52:49 -0500

    fixed reset btn

--- asset/script.js
+++ asset/script.js
@@ -7,6 +7,7 @@ let stopBtn = document.getElementById("stop");
 let resetBtn = document.getElementById("reset");
 let timer;
 let totalSeconds = 0;
+
 // =====================================
 input.addEventListener("input", function () {
   totalSeconds = parseInt(this.value);
@@ -17,6 +18,7 @@ input.addEventListener("input", function () {
     updateDisplay(totalSeconds);
   }
 });
+
 function updateDisplay(seconds) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
@@ -24,6 +26,7 @@ function updateDisplay(seconds) {
   hour.innerHTML = String(hours).padStart(2, "0");
   min.innerHTML = String(minutes).padStart(2, "0");
   second.innerHTML = String(secs).padStart(2, "0");
+
   // =============  change color ============
   if (seconds < 10) {
     document.querySelector(".time-display").classList.add("blink");
@@ -33,6 +36,7 @@ function updateDisplay(seconds) {
     document.querySelector(".time-display").style.color = "black";
   }
 }
+
 // ============= start btn ===============
 startBtn.addEventListener("click", function () {
   if (totalSeconds > 0) {
@@ -41,23 +45,33 @@ startBtn.addEventListener("click", function () {
     input.disabled = true;
   }
 });
+
 // =========== stop btn =================
 stopBtn.addEventListener("click", () => {
   clearInterval(timer);
   startBtn.disabled = false;
   input.disabled = false;
 });
+
 // ============= reset btn ============
-resetBtn.addEventListener("click", () => {
+resetBtn.addEventListener("click", function () {
   clearInterval(timer);
-  totalSeconds = 0;
-  updateDisplay(totalSeconds);
-  input.disabled = false;
+  totalSeconds = 0; // Reset totalSeconds
+  hour.innerHTML = `00`;
+  min.innerHTML = `00`;
+  second.innerHTML = `00`;
+  startBtn.disabled = false; // Re-enable start button
+  input.disabled = false; // Re-enable input
+  input.value = ""; // Clear input field
+  document.querySelector(".time-display").classList.remove("blink");
+  document.querySelector(".time-display").style.color = "black";
 });
+
 function timerHandler() {
   if (totalSeconds <= 0) {
     clearInterval(timer);
     startBtn.disabled = false;
+    input.disabled = false;
     document.querySelector(".time-display").classList.remove("blink");
     return;
   }
